59fd94c09cf53c35b5bbd0304c4ee8314e15a8f6
Amio <amio.cn@gmail.com>
2018-05-04 11:56:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# npm-why [![npm-version][npm-badge]][npm-link]
+
+## Install & Usage
+
+```bash
+npm install -g npm-why
+```
+
+```bash
+  npm-why - Identifies why a package has been installed.
+
+  Usage
+
+    $ npm-why <package>
+
+  Examples
+
+    $ npm-why babel-core
+```
+
+## License
+
+ISC @ Amio
+
+[amio-link]: https://github.com/amio
+[npm-badge]: https://img.shields.io/npm/v/npm-why.svg?style=flat-square
+[npm-link]: http://www.npmjs.com/package/npm-why
---
+++ index.js
@@ -0,0 +1,54 @@
+const logicalTree = require('npm-logical-tree')
+
+/**
+ * Find who depend upon a target package.
+ *
+ * @param {Object} pkg package.json
+ * @param {Object} pkgLock package-lock.json
+ * @param {String} target target package name
+ * @param {Function} printer
+ */
+module.exports = function (pkg, pkgLock, name, printer) {
+  const tree = logicalTree(pkg, pkgLock)
+
+  // Find target deps
+  const targets = new Set()
+  tree.forEach((dep, callback) => {
+    dep.name === name && targets.add(dep)
+    callback()
+  })
+
+  // Find requiredBy for those targets
+  const results = []
+  targets.forEach(t => {
+    lookupDependents(t, tree, [], results)
+  })
+  return results
+}
+
+function lookupDependents (target, root, breadcrumb = [], results) {
+  breadcrumb = breadcrumb.concat(target)
+
+  if (target.requiredBy.size === 0) {
+    return results.push(formatBreadcrumb(breadcrumb))
+  }
+
+  if (breadcrumb.includes(root)) {
+    return results.push(formatBreadcrumb(breadcrumb))
+  }
+
+  if (breadcrumb.slice(0, -1).includes(target)) {
+    return // Recursive dependency
+  }
+
+  target.requiredBy.forEach(dependent => {
+    lookupDependents(dependent, root, breadcrumb, results)
+  })
+}
+
+function formatBreadcrumb (bc) {
+  return bc.map(piece => ({
+    name: piece.name,
+    version: piece.version
+  }))
+}
