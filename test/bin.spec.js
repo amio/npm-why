39eacfa3a51e41dd7fd981ff75b4c7a21df396df
Amio <amio.cn@gmail.com>
2018-06-16 23:44:33 +0800

    Add tests

---
+++ test/bin.spec.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const execa = require('execa')
+const tap = require('tap')
+
+const cwd = path.resolve(__dirname, '..')
+const bin = path.join(cwd, 'bin/npm-why')
+const cli = (args, opts) => execa(bin, args, opts)
+
+tap.test('runs `--version`', async t => {
+  const packageJson = require(path.join(cwd, 'package.json'))
+  const { stdout } = await cli(['--version'])
+  t.is(stdout, packageJson.version, 'output version.')
+})
+
+tap.test('runs `chalk`', async t => {
+  const { stdout } = await cli(['chalk'], { cwd: __dirname })
+  t.is(stdout.trim(), `Who required chalk:
+
+  test > npm-why > chalk@2.4.1`, 'output correct result.')
+})
+
+// Issue #1
+tap.test('Exit 1 if no <package-name> provided', async t => {
+  const { code, stderr } = await cli([], { 'reject': false })
+  t.is(code, 1, 'exit code 1')
+  t.is(stderr.trim(), 'ERROR A <package-name> is required.', 'output hint.')
+})
