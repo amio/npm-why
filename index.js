45f2d9519a3a72efcdca58df4f6ca815cbdcda98
Amio Jin <amio.cn@gmail.com>
2020-11-09 16:24:31 +0800

    feat: support npm lockfile v2 (#124)
    * feat: support lockfile v2
    
    * test: add test for lockfile v2
    
    * chore: bump deps
    
    * chore: add @types/node
    
    * test: make standard happy
    
    * test: Travis CI => GitHub CI
    
    * chore: add root package info to output
    
    * chore: remove engine requirements

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
-const logicalTree = require('npm-logical-tree')
+const kleur = require('kleur')
+const Arborist = require('@npmcli/arborist')
 
 /**
- * Find who depend upon a target package.
+ * Run npm-why in a directory
  *
- * @param {Object} pkg package.json
- * @param {Object} pkgLock package-lock.json
- * @param {String} target target package name to lookup
+ * @param {String} dir Workding directory
+ * @param {String} packageName The package to lookup
  */
-module.exports = function (pkg, pkgLock, name) {
-  const tree = logicalTree(pkg, pkgLock)
+async function collectReasons (dir, packageName) {
+  const arb = new Arborist({ path: dir })
+  const tree = await arb.loadVirtual()
+  const pkgs = tree.children.keys()
 
-  // Find target deps
-  const targets = new Set()
-  tree.forEach((dep, callback) => {
-    dep.name === name && targets.add(dep)
-    callback()
-  })
+  let reasons = []
 
-  // Find requiredBy for those targets
-  const results = []
-  targets.forEach(t => {
-    lookupDependents(t, tree, [], results)
-  })
-  return results
+  // find all versions of <packageName>
+  for (const name of pkgs) {
+    if (name === packageName) {
+      const reasonChain = buildDependentsTree(tree.children.get(name))
+      reasons = reasons.concat(reasonChain.leafs)
+    }
+  }
+
+  // prepand root package info to every reason chain
+  const root = { name: tree.name, version: tree.version }
+  reasons = reasons.map(chain => chain.concat(root))
+
+  return reasons
 }
 
-function lookupDependents (target, root, breadcrumb = [], results) {
-  breadcrumb = breadcrumb.concat(target)
+function buildDependentsTree (node, paths = [], leafs = []) {
+  const { name, version, edgesIn } = node
 
-  if (target.requiredBy.size === 0) {
-    return results.push(formatBreadcrumb(breadcrumb))
+  // when leaf node
+  if (edgesIn.size === 0) {
+    leafs.push(paths)
+    return { name, version, leafs }
   }
 
-  if (breadcrumb.includes(root)) {
-    return results.push(formatBreadcrumb(breadcrumb))
+  // prevent dead loops
+  if (paths.includes(name)) {
+    return { name, version, leafs }
   }
 
-  if (breadcrumb.slice(0, -1).includes(target)) {
-    return // Recursive dependency
-  }
-
-  target.requiredBy.forEach(dependent => {
-    lookupDependents(dependent, root, breadcrumb, results)
+  const dependents = Array.from(edgesIn).map(edge => {
+    const pkg = { name, version }
+    return buildDependentsTree(edge.from, paths.concat(pkg), leafs)
   })
+
+  return { name, version, leafs, dependents }
 }
 
-function formatBreadcrumb (bc) {
-  return bc.map(piece => ({
-    name: piece.name,
-    version: piece.version
-  }))
+function printReasons (reasons, rootPackage) {
+  reasons.map(reason => {
+    const versionTag = kleur.dim('@' + reason[0].version)
+
+    const line = reason.reverse().map(pkg => {
+      return kleur.blue(pkg.name || rootPackage)
+    }).join(' > ')
+
+    return line + versionTag
+  }).sort().forEach(line => console.log(' ', line))
+}
+
+module.exports = {
+  collectReasons,
+  printReasons
 }
